d8b8e549ca916bd873b0a6074dff50489b074254
Mohamed Reda Ben Taouet <reda.bentaouet@student.odisee.be>
2026-02-12 14:14:48 +0100

    oefeningen gemaakt tot grid

---
+++ Sln1WpfLayout/WpfLayoutUIOefenblad/Exercises/ContentAlignment.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+using WpfLayoutUIOefenblad.Helpers;
+
+namespace WpfLayoutUIOefenblad.Exercises;
+
+[NavPage(title: "Content Alignment", description: "HorizontalContentAlignment, \nVerticalContentAlignment en TextAlign.", order: 3)]
+public partial class ContentAlignment : Page
+{
+    public ContentAlignment()
+    {
+        InitializeComponent();
+    }
+
+    private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+}
